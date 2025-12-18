a258a2902610b0233b2625447041e6a3d04f6724
Grilo2025 <introsmatenarq123456@gmail.com>
2025-12-18 00:24:31 -0300

    Update game scripts to use Kron Hub
    Replaced Mateus Hub scripts with Kron Hub scripts for supported games.

--- main.lua
+++ main.lua
@@ -1,20 +1,7 @@
-print("Mateus Hub iniciado")
-
-if game.PlaceId == 13822889 then -- Lumber Tycoon 2
-    loadstring(game:HttpGet(
-        "https://raw.githubusercontent.com/Grilo2025/MateusHub/main/LT2.lua"
-    ))()
-
-elseif game.PlaceId == 606849621 then -- Jailbreak
-    loadstring(game:HttpGet(
-        "https://raw.githubusercontent.com/Grilo2025/MateusHub/main/Jailbreak.lua"
-    ))()
-
-elseif game.PlaceId == 185655149 then -- Bloxburg
-    loadstring(game:HttpGet(
-        "https://raw.githubusercontent.com/Grilo2025/MateusHub/main/Bloxburg.lua"
-    ))()
-
-else
-    warn("Mateus Hub | Jogo não suportado")
+if game.PlaceId == 13822889 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/DevKron/Kron_Hub/refs/heads/main/lt2new'))("")
+elseif game.PlaceId == 1537690962 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/DevKron/Kron_Hub/refs/heads/main/bss'))("")
+elseif game.PlaceId == 189707 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/DevKron/Kron_Hub/refs/heads/main/nds'))("")
 end
