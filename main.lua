949f83cae7c9e85dfc505fcf12ca04d66d23a8d2
Grilo2025 <introsmatenarq123456@gmail.com>
2025-12-18 00:16:18 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,20 @@
-print("Mateus Hub carregado com sucesso!")
+print("Mateus Hub iniciado")
+
+if game.PlaceId == 13822889 then -- Lumber Tycoon 2
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/Grilo2025/MateusHub/main/LT2.lua"
+    ))()
+
+elseif game.PlaceId == 606849621 then -- Jailbreak
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/Grilo2025/MateusHub/main/Jailbreak.lua"
+    ))()
+
+elseif game.PlaceId == 185655149 then -- Bloxburg
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/Grilo2025/MateusHub/main/Bloxburg.lua"
+    ))()
+
+else
+    warn("Mateus Hub | Jogo não suportado")
+end
